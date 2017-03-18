17804d12df4a24acca3d3d268fb1b07ad93134e8
Pedro Ivo <pedroivo-hp@hotmail.com>
2017-03-17 22:04:23 -0300

    Fim de PAVI

--- src/pavi/melhoramigo/bean/AdminBean.java
+++ src/pavi/melhoramigo/bean/AdminBean.java
@@ -1,28 +1,19 @@
 package pavi.melhoramigo.bean;
 
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
 
 import org.primefaces.context.RequestContext;
 
-import pavi.melhoramigo.bo.CadastroUsuarioBO;
-import pavi.melhoramigo.bo.LoginUsuarioBO;
 import pavi.melhoramigo.dao.SolicitacaoAdocaoDAO;
-import pavi.melhoramigo.dao.UsuarioDAO;
 import pavi.melhoramigo.dao.conexao.ConexaoBase;
-import pavi.melhoramigo.vo.CaoVO;
 import pavi.melhoramigo.vo.SolicitacaoAdocaoVO;
 
 @ManagedBean
 public class AdminBean extends ConexaoBase {
-	//private SolicitacaoAdocaoVO solicit = new SolicitacaoAdocaoVO();
 	private List<SolicitacaoAdocaoVO> solicitacoes = new ArrayList<>();
 
 	public List<SolicitacaoAdocaoVO> getSolicitacoes() {
